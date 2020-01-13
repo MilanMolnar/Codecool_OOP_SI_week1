279a2612c37af7174a04505d7584c0d09c87a2f7
MilanMolnar <54602124+MilanMolnar@users.noreply.github.com>
2020-01-13 04:11:00 -0800

    Uploading missing exercises.

---
+++ HelloWorld_exercise/HelloWorld_Modified/HelloWorld_Modified/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace HelloWorld_Modified
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int argCount = args.Length;
+            if (argCount != 0)
+            {
+                for (int i = 0; i < argCount; i++)
+                {
+                    Console.WriteLine($"Hello {args[i]}!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Hello world!");
+            }
+        }
+    }
+}
